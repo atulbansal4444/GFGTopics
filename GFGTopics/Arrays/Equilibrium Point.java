fe8c2d4dec68bee24e720e2e56f1a99e25ebf75a
Atul Bansal <30015420+atulbansal4444@users.noreply.github.com>
2021-09-23 20:40:40 +0530

    Arrays Q added

---
+++ GFGTopics/Arrays/Equilibrium Point.java
@@ -0,0 +1,83 @@
+/*
+Given an array A of n positive numbers. The task is to find the first Equilibium Point in the array. 
+Equilibrium Point in an array is a position such that the sum of elements before it is equal to the sum of elements after it.
+
+Example 1:
+
+Input: 
+n = 5 
+A[] = {1,3,5,2,2} 
+Output: 3 
+Explanation: For second test case 
+equilibrium point is at position 3 
+as elements before it (1+3) = 
+elements after it (2+2). 
+ 
+
+Example 2:
+
+Input:
+n = 1
+A[] = {1}
+Output: 1
+Explanation:
+Since its the only element hence
+its the only equilibrium point.
+ 
+
+Your Task:
+The task is to complete the function equilibriumPoint() which takes the array and n as input parameters and returns the point of equilibrium. Return -1 if no such point exists.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+class Solution
+{
+    // a: input array
+    // n: size of array
+    public static int equilibriumPoint(long arr[], int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+        else if (n == 2) return -1;
+        long []front = new long[n];
+        long []back = new long[n];
+        
+        for (int i=0; i<n; i++)
+        {
+            if (i !=0 )
+            {
+                front[i] = front[i-1] + arr[i];
+            }
+            else
+            {
+                front[i] = arr[i];
+            }
+        }
+        for (int i = n-1; i>0; i--)
+        {
+            if (i <= n-2)
+            {
+                back[i] = back[i+1] + arr[i];
+            }
+            else
+            {
+                back[i] = arr[i];
+            }
+        }
+        for(int i=0; i<n; i++)
+        {
+            if (front[i]==back[i])
+                return i+1;
+        }
+        return -1;
+        
+    }
+}
---
+++ GFGTopics/Arrays/Leaders in an array.java
@@ -0,0 +1,55 @@
+/*
+Given an array A of positive integers. Your task is to find the leaders in the array. An element of array is leader if it is greater than or equal to all the elements to its right side. The rightmost element is always a leader. 
+
+Example 1:
+Input:
+n = 6
+A[] = {16,17,4,3,5,2}
+Output: 17 5 2
+Explanation: The first leader is 17 
+as it is greater than all the elements
+to its right.  Similarly, the next 
+leader is 5. The right most element 
+is always a leader so it is also 
+included.
+ 
+Example 2:
+Input:
+n = 5
+A[] = {1,2,3,4,0}
+Output: 4 0
+
+Your Task:
+You don't need to read input or print anything. The task is to complete the function leader() which takes array A and n as input parameters and returns an array of leaders in order of their appearance.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+*/
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    // Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int max = arr[n - 1];
+        list.add(arr[n - 1]);
+
+        // for array size 1 directly returning the list with 1 element.
+        if (n == 1) {
+            return list;
+        }
+
+        // traverse from back and check if the max is less than the arr[i] chnge max and
+        // push it in list.
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= max) {
+                max = arr[i];
+                list.add(max);
+            }
+        }
+        Collections.reverse(list);
+        return list;
+    }
+}
