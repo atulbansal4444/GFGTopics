89b82d64cd9c3ec1cbab7fc55b533f40b1dba140
Atul <atulbansal4444@gmail.com>
2021-10-12 13:23:16 +0530

    Changes: * adding main to each file and formatting.

--- GFGTopics/Arrays/EquilibriumPoint.java
+++ GFGTopics/Arrays/EquilibriumPoint.java
@@ -1,5 +1,5 @@
 /*
-Given an array A of n positive numbers. The task is to find the first Equilibium Point in the array. 
+Given an array A of n positive numbers. The task is to find the first Equilibrium Point in the array.
 Equilibrium Point in an array is a position such that the sum of elements before it is equal to the sum of elements after it.
 
 Example 1:
@@ -21,8 +21,8 @@ n = 1
 A[] = {1}
 Output: 1
 Explanation:
-Since its the only element hence
-its the only equilibrium point.
+Since it's the only element hence
+it's the only equilibrium point.
  
 
 Your Task:
@@ -34,36 +34,56 @@ Expected Auxiliary Space: O(1)
 
 package Arrays;
 
-class EquilibriumPoint {
-    // a: input array
-    // n: size of array
-    public static int equilibriumPoint(long arr[], int n) {
-        if (n == 1) {
+public class EquilibriumPoint
+{
+    public static int equilibriumPoint(long[] arr, int n)
+    {
+        if (n == 1)
+        {
             return 1;
-        } else if (n == 2)
+        }
+        else if (n == 2)
+        {
             return -1;
+        }
         long[] front = new long[n];
         long[] back = new long[n];
 
-        for (int i = 0; i < n; i++) {
-            if (i != 0) {
+        for (int i = 0; i < n; i++)
+        {
+            if (i != 0)
+            {
                 front[i] = front[i - 1] + arr[i];
-            } else {
+            }
+            else
+            {
                 front[i] = arr[i];
             }
         }
-        for (int i = n - 1; i > 0; i--) {
-            if (i <= n - 2) {
+        for (int i = n - 1; i > 0; i--)
+        {
+            if (i <= n - 2)
+            {
                 back[i] = back[i + 1] + arr[i];
-            } else {
+            }
+            else
+            {
                 back[i] = arr[i];
             }
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             if (front[i] == back[i])
+            {
                 return i + 1;
+            }
         }
         return -1;
+    }
 
+    public static void main(String[] args)
+    {
+        long[] A = {1,3,5,2,2};
+        System.out.println(equilibriumPoint(A, A.length));
     }
 }
