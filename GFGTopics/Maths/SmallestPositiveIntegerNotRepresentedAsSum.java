d59d1dd2cdbc60976e12ce7b7d9e9da849dbc6f8
Atul <atulbansal4444@gmail.com>
2021-10-25 17:41:06 +0530

    formatting

--- GFGTopics/Maths/SmallestPositiveIntegerNotRepresentedAsSum.java
+++ GFGTopics/Maths/SmallestPositiveIntegerNotRepresentedAsSum.java
@@ -21,12 +21,12 @@ Explanation:
 1 is present in the array. 
 2 can be created by combining two 1s.
 3 can be created by combining three 1s.
-4 is the smallest integer value that cannot be 
-represented as sum of elements from the array.
+4 is the smallest integer value that cannot be represented as sum of elements from the array.
 
 Your Task:  
-You dont need to read input or print anything. Complete the function smallestpositive() which takes the array and N as input parameters and returns the smallest positive integer value that cannot be represented as sum of some elements from the array.
-
+You don't need to read input or print anything. Complete the function smallestPositive() which takes the array and N as
+input parameters and returns the smallest positive integer value that cannot be represented as sum of some elements
+from the array.
 
 Expected Time Complexity: O(N * Log(N))
 Expected Auxiliary Space: O(1)
